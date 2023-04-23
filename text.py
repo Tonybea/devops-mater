04f095efaa927c52aec9511cb72dde5bede8dd4d
Anthony Osei <assibey012@gmail.com>
2023-04-23 22:07:09 +0000

    deleted a line

--- text.py
+++ text.py
@@ -1,10 +1,10 @@
-thislist = ["apple", "banana", "cherry"]
+thislist ="apple", "banana", "cherry"]
 if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list")
 
 
 thislist = ["apple", "banana", "cherry"]
-i = 0
+
 while i < len(thislist):
   print(thislist[i])
   i = i + 1
